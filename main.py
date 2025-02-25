7b0f40b33e359556996ca2137ba9c8333d33579f
26nch <nchb1807838@gmail.com>
2025-02-25 15:17:26 -0800

    Init

---
+++ main.py
@@ -0,0 +1,51 @@
+# BASIC
+# 0. Install and setup Python
+# 1. print()
+# 2. Variables
+# 3. Arthimetic & Math
+# 4. Data Type and Typecasting
+# 5. User input()
+# 6. String methods
+# 7. String indexing
+# 8. Format specifier
+# 9. if statements
+# 10. Logical Operators
+# 11. For loops
+# 12. While loops
+# 13. Nested loops
+# 14. Lits, Tuples, Sets
+# 15. Dictionaries
+# 16. Random Number
+# 17. Functions
+# 18. Default arguments 
+# 19. Keyword arguments
+# 20. *args & **kwargs
+# 21. Iterables 
+# 22. Membership operators 
+# 23. List comprehensions 
+# 24. Match-case statements 
+# 25. Modules 
+# 26. Scope resolution
+# 27. if name == 'main':
+# 28. File detection 
+# 29. Writing files 
+# 30. Reading files 
+# 31. Dates & times 
+# 32. Exception handling 
+
+# ADVANDED
+# 33. Python Object Oriented Programming 
+# 34. Class variables 
+# 35. Inheritance 
+# 36. Multiple inheritance 
+# 37. super() 
+# 38. Polymorphism 
+# 39. Duck typing 
+# 40. Static methods 
+# 41. Class methods 
+# 42. Magic methods 
+# 43. @property 
+# 44. Decorators 
+# 45. Multithreading 
+# 46. Request API data 
+
